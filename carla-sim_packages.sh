9bff4a6fa3395db853f15275b8c34a1d0fb8c781
jswoo <dnwnstkd122@gmail.com>
2023-04-23 11:42:43 +0900

    환경변수 등록 추가 및 파이썬 패키지 설치 추가

--- carla-sim_packages.sh
+++ carla-sim_packages.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # 메인 경로 설정
-mkdir -p /home/${USER}/Desktop/AGC/Carla-Simulator /home/${USER}/Desktop/AGC/autoware-ai /home/${USER}/Desktop/AGC/Carla-Leaderboard /home/${USER}/Desktop/AGC/Carla-Scenario_Runner
+mkdir -p /home/${USER}/Desktop/AGC/Carla-Simulator \
+         /home/${USER}/Desktop/AGC/autoware-ai  \
+         /home/${USER}/Desktop/AGC/Carla-Leaderboard \
+         /home/${USER}/Desktop/AGC/Carla-Scenario_Runner
 echo "export AGC_ROOT=/home/${USER}/Desktop/AGC/" >> ~/.bashrc
 source ~/.bashrc
 
@@ -35,25 +38,34 @@ pip install --user wheel &&
 pip3 install --user wheel auditwheel
 
 
-# Carla Leaderboard 패키지 설치
+# Carla 시뮬레이터 설치
 cd /home/${USER}/Desktop/AGC/Carla-Simulator
 wget -P ~/AGC/Carla-Leaderboard_2_0 https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/Leaderboard/CARLA_Leaderboard_20.tar.gz
 tar -xvfz CARLA_Leaderboard_20.tar.gz
 
 # 환경변수 설정
 echo "export CARLA_ROOT=${AGC_ROOT}/Carla-Leaderboard_2_0" >> ~/.bashrc
-echo " " >> ~/.bashrc
+
+echo "export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI" >> ~/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla " >> ~/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla/agents" >> ~/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla/dist/carla-$CARLA_VERSION-py2.7-linux-x86_64.egg" >> ~/.bashrc    # ROS Melodic은 Python2.x를 사용, Python3.x로 추후 변경 예정
+echo "export CARLA_SERVER=~/carla_simulator/CARLA_$CARLA_VERSION/CarlaUE4.sh" >> ~/.bashrc
 
 # Carla Leaderboard 설치
 cd /home/${USER}/Desktop/AGC/Carla-Leaderboard
 git clone -b leaderboard-2.0 --single-branch https://github.com/carla-simulator/leaderboard.git
-
-# 환경변수 설정
-echo "source LEADERBOARD_ROOT=~/${CARLA_ROOT}/leaderboard" >> ~/.bashrc
-cd ${}
-
+pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Carla Scenario Runner 설치
 cd /home/${USER}/Desktop/AGC/Carla-Scenario_Runner
+git clone -b leaderboard-2.0 --single-branch https://github.com/carla-simulator/scenario_runner.git
+pip install -r requirements.txt
+pip3 install -r requirements.txt
+
+# 환경변수 등록
+echo "export PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/":"${SCENARIO_RUNNER_ROOT}":"${LEADERBOARD_ROOT}":"${CARLA_ROOT}/PythonAPI/carla/dist/carla-0.9.13-py2.7-linux-x86_64.egg":${PYTHONPATH}" << ~/.bashrc     # 추후 Python3.x로 진행 예정
 
 exit 0
+
