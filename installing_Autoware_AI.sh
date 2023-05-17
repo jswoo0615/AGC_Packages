6a89b5768d967a76ba3fb9a7b922d2bb24909cf4
jswoo <dnwnstkd122@gmail.com>
2023-05-17 09:02:39 +0900

    Autoware_AI 설치 스크립트 내용 추가

--- installing_Autoware_AI.sh
+++ installing_Autoware_AI.sh
@@ -1,6 +1,5 @@
 cd /home/${USER}/Desktop/AGC/
 git clone https://github.com/jswoo0615/AGC_Packages.git
-cd /home/${USER}/Desktop/AGC/AGC_Packages
 mv AGC_Packages AGC_Autoware_AI
 git checkout Autoware_AI
 
@@ -10,3 +9,5 @@ vcs import src < AGC_Autoware.repos
 
 sudo rosdep init
 rosdep update
+
+colcon build
